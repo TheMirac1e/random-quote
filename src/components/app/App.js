5222c43d4035457edc254c3080a085763ba28287
Even Koshliak <evenkoshliak@o-s-i.org>
2022-08-24 22:48:45 +0300

    CONFIG - added sass, changed gitignore

---
+++ src/components/app/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Component } from "react";
+import Card from "./components/card/card";
+
+class App extends Component {
+  render() {
+    return (
+        <Card/>
+    );
+  }
+}
+
+export default App;
