39798a18a3e6f9d9efe91129234344f9cdd4c278
Even Koshliak <evenkoshliak@o-s-i.org>
2022-08-24 23:17:57 +0300

    [FEATURE] - JS/CSS - add styles to static card

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -8,15 +8,15 @@ class Card extends Component{
       <article className={'card-item'}>
         <div className={"card-item__wrapper"}>
           <div className={"card-item__quote-wrap"}>
-            <h2 className={'card-item__quote'}>Life isn’t about getting and having, it’s about giving and being.</h2>
+            <h2 className={'card-item__quote'}><i className="fa fa-quote-left"> </i>Life isn’t about getting and having, it’s about giving and being.</h2>
             <span className={'card-item__author'}><span>- </span>Kevin Kruse</span>
           </div>
           <div className="card-item__info">
             <div className="card-item__social">
-              <a href="https://twitter.com" target={"_blank"} rel='noreferrer'><i className="fa fa-quote-left"> </i></a>
-              <a href="https://www.facebook.com/" target={"_blank"} rel='noreferrer'>facebook</a>
+              <a className={'card-item__social-item'} href="https://twitter.com" target={"_blank"} rel='noreferrer'><i className="fa fa-twitter"></i></a>
+              <a className={'card-item__social-item'} href="https://www.facebook.com/" target={"_blank"} rel='noreferrer'><i className="fa fa-facebook"></i></a>
             </div>
-            <button type={"button"}>New quote</button>
+            <button className={'card-item__random-quote'} type={"button"}>New quote</button>
           </div>
         </div>
       </article>
